2d45ff935da126d94c5c753b856c0e399c9b2ac6
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 09:51:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-#module "iam-eks-role" {
-#  source  = "terraform-aws-modules/iam/aws//examples/iam-eks-role"
-#  version = "5.28.0"
-#}
+module "iam-eks-role" {
+  source  = "terraform-aws-modules/iam/aws//examples/iam-eks-role"
+  version = "5.28.0"
+}
 
 resource "aws_iam_user" "test_svc_accts" {
   for_each = toset(var.svc_accts)
