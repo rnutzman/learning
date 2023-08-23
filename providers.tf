6a9a38560f87adaa31aeecdf04854f513a34c314
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 09:00:17 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,8 +11,8 @@ terraform {
 provider "aws" {
   region = var.aws_region
   
-  shared_config_files      = ["/Users/Ron/.aws/config"]
-  shared_credentials_files = ["/Users/Ron/.aws/credentials"]
-  profile                  = "default"
+#  shared_config_files      = ["/Users/Ron/.aws/config"]
+#  shared_credentials_files = ["/Users/Ron/.aws/credentials"]
+#  profile                  = "default"
 
 }
