419f036310c88b07275a4214bd88d206b07eb7a7
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 14:11:27 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,10 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 3.0"
+      version = ">= 4.0"
     }
   }
 }
