0ecc803b2da4e43f3a1856723c6c9c264d58546b
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 13:32:49 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "role_arn" {
+  description = "ARN for the eks role"
   #value = iam-eks-role.iam_role_arn
   value = example_resource.example.my_eks_role_arn
 }
 
 output "user_arns" {
+  description = "ARNs for created users"
   value = [ for accts in aws_iam_user.test_svc_accts: accts.arn]
 }
 
