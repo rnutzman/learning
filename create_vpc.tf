aa6992defae7fec4cb1e26e7ac3389aa33ffb30f
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 18:28:57 -0400

    Create create_vpc.tf

---
+++ create_vpc.tf
@@ -0,0 +1,49 @@
+resource "aws_vpc" "eks-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name    = "EKS-VPC"
+    POC     = "Ron"
+    Project = "Learning"
+  }
+}
+
+resource "aws_subnet" "eks-subnet-1" {
+  vpc_id     = aws_vpc.eks-vpc.id
+  cidr_block = "10.0.1.0/24"
+  
+  availability_zone_id = "us-east-2a"
+  
+  tags = {
+    Name = "eks-subnet-1"
+    VPC  = aws_vpc.eks-vpc.name
+    AZ   = "us-east-2a"
+  }
+}
+
+resource "aws_subnet" "eks-subnet-2" {
+  vpc_id      = aws_vpc.eks-vpc.id
+  cidr_block  = "10.0.2.0/24"
+  
+  availability_zone_id = "us-east-2b"
+  
+  tags = {
+    Name = "eks-subnet-2"
+    VPC  = aws_vpc.eks-vpc.name
+    AZ   = "us-east-2b"
+  }
+}
+
+resource "aws_subnet" "eks-subnet-3" {
+  vpc_id     = aws_vpc.eks-vpc.id
+  cidr_block = "10.0.3.0/24"
+  
+  availability_zone_id = "us-east-2c"
+  
+  tags = {
+    Name = "eks-subnet-3"
+    VPC  = aws_vpc.eks-vpc.name
+    AZ   = "us-east-2c"
+  }
+}
