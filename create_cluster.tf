6603268df214c32a08da10b080fb6997f2b63705
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 18:41:48 -0400

    Update create_cluster.tf

--- create_cluster.tf
+++ create_cluster.tf
@@ -10,3 +10,26 @@ resource "aws_eks_cluster" "my-eks-cluster" {
   aws_iam_role.eks-iam-role,
  ]
 }
+
+resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name  = aws_eks_cluster.my-eks-cluster.name
+  node_group_name = "my-eks-cluster-workernodes"
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
+  instance_types = ["t3.micro"]
+ 
+  scaling_config {
+   desired_size = 1
+   max_size   = 1
+   min_size   = 1
+  }
+ 
+  depends_on = [
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+ }
+
+  ]
+ }
