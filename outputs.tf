f2d86fe3338c5d859e5191166c0ba258dd5e585e
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 19:15:16 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "eks_cluster_role_arn" {
   description = "ARN for the eks cluster role"
-  value = aws_iam_role.eks-cluster-iam-role.iam_role_arn
+  value = aws_iam_role.eks-cluster-iam-role.arn
 }
 
 output "eks_workernode_role_arn" {
   description = "ARN for the eks workernode role"
-  value = aws_iam_role.eks-workernode-iam-role.iam_role_arn
+  value = aws_iam_role.eks-workernode-iam-role.arn
 }
 
 #output "user_arns" {
