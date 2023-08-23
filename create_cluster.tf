2be3fb91dd1cee84dac2db251255b189d520542c
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 19:01:20 -0400

    Update create_cluster.tf

--- create_cluster.tf
+++ create_cluster.tf
@@ -11,17 +11,22 @@ resource "aws_eks_cluster" "my-eks-cluster" {
  ]
 }
 
+data "aws_ssm_parameter" "eks_ami_release_version" {
+  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.my-eks-cluster.version}/amazon-linux-2/recommended/release_version"
+}
+
 resource "aws_eks_node_group" "worker-node-group" {
   cluster_name  = aws_eks_cluster.my-eks-cluster.name
   node_group_name = "my-eks-cluster-workernodes"
   node_role_arn  = aws_iam_role.eks-workernode-iam-role.arn
   subnet_ids   = [aws_subnet.eks-subnet-1.id,aws_subnet.eks-subnet-2.id, aws_subnet.eks-subnet-3.id]
+  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
   instance_types = ["t3.micro"]
  
   scaling_config {
-   desired_size = var.desired-size
-   max_size   = var.max-size
-   min_size   = var.min-size
+   desired_size = var.asg.desired-size
+   max_size   = var.asg.max-size
+   min_size   = var.asg.min-size
   }
  
   depends_on = [
