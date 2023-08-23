19b4e64a73ec443692519bb2ee7c959f9e34797a
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-23 18:39:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "eks-iam-role" {
+resource "aws_iam_role" "eks-cluster-iam-role" {
  name = "eks-iam-role"
  path = "/"
 
@@ -20,27 +20,60 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
- role       = aws_iam_role.eks-iam-role.name
+ role       = aws_iam_role.eks-cluster-iam-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
- role       = aws_iam_role.eks-iam-role.name
+ role       = aws_iam_role.eks-cluster-iam-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks-iam-role.name
+  role       = aws_iam_role.eks-cluster-iam-role.name
 }
 
 
+resource "aws_iam_role" "eks-workernode-iam-role" {
+  name = "eks-node-group"
+ 
+  assume_role_policy = jsonencode({
+   Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+     Service = "ec2.amazonaws.com"
+    }
+   }]
+   Version = "2012-10-17"
+  })
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role    = aws_iam_role.eks-workernode-iam-role.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role    = aws_iam_role.eks-workernode-iam-role.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.eks-workernode-iam-role.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role    = aws_iam_role.eks-workernode-iam-role.name
+ }
 
 
-resource "aws_iam_user" "test_svc_accts" {
-  for_each = toset(var.svc_accts)
-
-  name = each.key
-}
+#resource "aws_iam_user" "test_svc_accts" {
+#  for_each = toset(var.svc_accts)
+#  name = each.key
+#}
 
 #resource "iam-eks-role" "example" {
 # my_eks_role_arn = module.iam-eks-role.iam_role_arn
