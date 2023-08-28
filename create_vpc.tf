5b261342e86a721b5b51376ef96c05582f769dde
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-28 09:27:53 -0400

    Update create_vpc.tf

--- create_vpc.tf
+++ create_vpc.tf
@@ -21,36 +21,33 @@ resource "aws_subnet" "eks-subnets" {
   availability_zone_id    = lookup(var.aws_subnet_az, var.aws_subnets[count.index])
   map_public_ip_on_launch = false
 
-  tags = concat(
-    [
-      {
-        "key"                 = "Name"
-        "value"               = "${var.aws_subnets[count.index]}"
-        "propagate_at_launch" = true
-      },
-      {
-        "key"                 = "VPC"
-        "value"               = "${aws_vpc.eks-vpc.id}"
-        "propagate_at_launch" = true
-      },
-      {
-        "key"                 = "AZ"
-        "value"               = "${lookup(var.aws_subnet_az, var.aws_subnets[count.index])}"
-        "propagate_at_launch" = true
-      },
-      {
-        "key"                 = "kubernetes.io/role/internal-elb"
-        "value"               = "1"
-        "propagate_at_launch" = true
-      },
-      {
-        "key"                 = "kubernetes.io/cluster/${var.cluster_name}"
-        "value"               = "shared"
-        "propagate_at_launch" = true
-      }
-    ],
-	var.default_tags,
-  )
+  tags = [
+    {
+      "key"                 = "Name"
+      "value"               = "${var.aws_subnets[count.index]}"
+      "propagate_at_launch" = true
+    },
+    {
+      "key"                 = "VPC"
+      "value"               = "${aws_vpc.eks-vpc.id}"
+      "propagate_at_launch" = true
+    },
+    {
+      "key"                 = "AZ"
+      "value"               = "${lookup(var.aws_subnet_az, var.aws_subnets[count.index])}"
+      "propagate_at_launch" = true
+    },
+    {
+      "key"                 = "kubernetes.io/role/internal-elb"
+      "value"               = "1"
+      "propagate_at_launch" = true
+    },
+    {
+      "key"                 = "kubernetes.io/cluster/${var.cluster_name}"
+      "value"               = "shared"
+      "propagate_at_launch" = true
+    }
+  ]
 }
 
 
