37611614971ee10af2dc8521955b5ef00d582523
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-28 12:38:17 -0400

    Update create_cluster.tf

--- create_cluster.tf
+++ create_cluster.tf
@@ -59,6 +59,7 @@ resource "aws_eks_node_group" "worker-node-group" {
   instance_types  = ["t2.micro"]
 
   remote_access {
+    ec2_ssh_key = var.ec2_ssh_key
     source_security_group_ids = [aws_security_group.eks-cluster-sg.id]
   }
 
